707298c5b52f5a5e01d5e309e346214b1604691f
菊本　知花 <motyu20@kikumotochibananoMacBook-Air.local>
2020-05-12 15:41:21 +0900

    tweetapp

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,7 @@
 class LikesController < ApplicationController
-  # before_actionに「:authenticate_user」を追加してください
   before_action :authenticate_user
   
-  # createアクションを追加してください
+
   def create
   	@like = Like.new(
   		user_id: @current_user.id,
