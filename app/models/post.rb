a79562fb14d1ac17b73c34a10d3259121d1babf6
菊本　知花 <motyu20@kikumotochibananoMacBook-Air.local>
2020-05-16 16:54:20 +0900

    tweetapp

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   validates :content, {presence: true, length: {maximum: 500}}
   validates :user_id, {presence: true}
-  mount_uploader :post_image, ImageUploader
 
   def user
     return User.find_by(id: self.user_id)
